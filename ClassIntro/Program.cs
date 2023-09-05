5553a03bc7f00d83bd9c0f58b8de18d053930a7e
Hande K <Hande K@DESKTOP-9J2RDDU>
2023-09-05 14:45:49 +0300

    Proje dosyası ekle.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassIntro
+{
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int İzlenmeOrani { get; set; }
+
+        static void Main(string[] args)
+        {            
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.İzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Kerem Varis";
+            kurs2.İzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Pyton";
+            kurs3.Egitmen = "Berkay Bilgin";
+            kurs3.İzlenmeOrani = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Murat Kurtboğan";
+            kurs4.İzlenmeOrani = 100;
+
+            Console.WriteLine(kurs3.KursAdi + " \n" + kurs3.Egitmen + " \n "+kurs3.İzlenmeOrani );
+        }
+    }
+}
